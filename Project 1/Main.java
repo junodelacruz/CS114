59b8fde2218326309cf96ed8f82cdaf2660b5a30
Juno Dela Cruz <55854673+Anetryst@users.noreply.github.com>
2024-09-25 03:01:19 +0000

    second commit

--- Project 1/Main.java
+++ Project 1/Main.java
@@ -1,37 +1,53 @@
-import java.util.Scanner;
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
+    private static int rows;
+    private static int columns;
+    private static char[][] maze;
     public static void main(String args[]){
-       try(Scanner maze = new Scanner(new File("maze.dat"))) {
-        int row = 0;
-        int column = 0;
-        // Finding the coordinates of the entrance
-        while(maze.hasNextLine()){
-                String line = maze.nextLine();
-                System.out.println(line);
-                if(!line.contains("+")){
-                    row++;
-                }
-                else {
-                    for(int i = 0; i<line.length();i++) {
-                        if(line.charAt(i) == '+'){
-                            break;
-                        }
-                        else {
-                            column++;
-                        }
-                    }
-                    break;
-                }
-        }
-        System.out.println(row + " " +  column);
-       } catch (FileNotFoundException error) {
+        try(BufferedReader read = new BufferedReader(new FileReader("maze.dat"))) {
+            //Create 2D array for maze
+            String line = read.readLine();
+            String[] firstLine = line.split(" ");
+            rows = Integer.parseInt(firstLine[0]);
+            columns = Integer.parseInt(firstLine[1]);
+            maze = new char[rows][columns];
+            
+            for(int i = 0; i < rows; i++){
+                line = read.readLine();
+                maze[i] = line.toCharArray();
+            }
+
+            //Entering point
+            findEntrance(maze);
+
+            //Traversing the 2D maze
+            // traverse(findEntrance(maze)[0], find);
+      
+
+       }catch (IOException error) {
             System.out.println(error);
        }
 
-       //Moving through the maze
     }
+
+    public static int[] findEntrance(char[][] maze){
+        int[] start = new int[2];
+        for(int i = 0; i < maze.length; i++){
+            for(int j = 0; j < maze[i].length; j++){
+                if(maze[i][j] == '+'){
+                    start[0] = i;
+                    start[1] = j;
+                }
+            }
+        }
+        return start;
+    }
+
+    // public static boolean traverse(){
+
+    // }
     
 }
