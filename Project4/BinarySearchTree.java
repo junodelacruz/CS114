96fd4e83599b6c2414e190a687f4f5f51191cf2e
anetryst <juno.delacruz0124@gmail.com>
2024-12-06 00:18:01 -0500

    project 4 implementation and notes

--- Project4/BinarySearchTree.java
+++ Project4/BinarySearchTree.java
@@ -5,19 +5,34 @@ public class BinarySearchTree<E extends Comparable<? super E>> extends BinaryTre
     public void insert(E data){
         root = insertMethod(root, data);
     }
-    public E insertMethod(Node<E> curr, E data){
+    public Node<E> insertMethod(Node<E> curr, E data){
+        // empty curr
         if(curr == null){
-            return data;
+            return new Node<E>(data);
         }
-        
+        // value is less
+        else if(data.compareTo(curr.data) < 0){
+            // recursion
+            return insertMethod(curr.left, data);
+        }
+        // value is greater than or equal
+        else {
+            // recursion
+            return insertMethod(curr.right, data);
+        }
     }
     public void remove(E data){
-               
+        root = removeMethod(root, data);
+    }
+    public Node<E> removeMethod(Node<E> curr, E data){
+        if(curr.data == data){
+            //return findIOP() <-- returns a node
+        }
     }
     public int search(E data){
-               
+        return 1;
     }
     public Iterator<E> iterator(){
-
+        
     }
 }
